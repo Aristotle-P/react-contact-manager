e04d6022c91090ab9048f11943a7ecd1d48f82f8
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-12-08 22:21:19 -0500

    more add contact functionality

--- src/context.js
+++ src/context.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 const Context = React.createContext();
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "DELETE_CONTACT":
+    case 'DELETE_CONTACT':
       return {
         ...state,
         contacts: state.contacts.filter(
           contact => contact.id !== action.payload
         )
+      };
+    case 'ADD_CONTACT':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
       };
     default:
       return state;
@@ -21,21 +26,21 @@ export class Provider extends Component {
     contacts: [
       {
         id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
       },
       {
         id: 2,
-        name: "Karen Williams",
-        email: "karen@gmail.com",
-        phone: "222-222-2222"
+        name: 'Karen Williams',
+        email: 'karen@gmail.com',
+        phone: '222-222-2222'
       },
       {
         id: 3,
-        name: "Henry Johnson",
-        email: "henry@gmail.com",
-        phone: "111-111-1111"
+        name: 'Henry Johnson',
+        email: 'henry@gmail.com',
+        phone: '111-111-1111'
       }
     ],
     dispatch: action => this.setState(state => reducer(state, action))
