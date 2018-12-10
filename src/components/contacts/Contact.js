f1e5b3e1339eab4170cbd8d76265dec46193728b
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-12-10 01:15:16 -0500

    finished crud functionality with react router and axios

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   };
 
-  onDeleteClick = (id, dispatch) => {
-    dispatch({ type: 'DELETE_CONTACT', payload: id });
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    } catch (e) {
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }
   };
 
   render() {
@@ -19,7 +26,7 @@ class Contact extends Component {
         {value => {
           const { dispatch } = value;
           return (
-            <div className='card card-body mb-3'>
+            <div className="card card-body mb-3">
               <h4>
                 {name}{' '}
                 <i
@@ -28,19 +35,30 @@ class Contact extends Component {
                       showContactInfo: !this.state.showContactInfo
                     })
                   }
-                  className='fas fa-sort-down'
+                  className="fas fa-sort-down"
                   style={{ cursor: 'pointer' }}
                 />
                 <i
-                  className='fas fa-times'
+                  className="fas fa-times"
                   style={{ cursor: 'pointer', float: 'right', color: 'red' }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      float: 'right',
+                      color: 'black',
+                      marginRight: '1rem'
+                    }}
+                  />
+                </Link>
               </h4>
               {showContactInfo ? (
-                <ul className='list-group'>
-                  <li className='list-group-item'>Email: {email}</li>
-                  <li className='list-group-item'>Phone: {phone}</li>
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
                 </ul>
               ) : null}
             </div>
