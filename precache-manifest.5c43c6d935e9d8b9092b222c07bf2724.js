783da9f7c131689d5b2c854f439ccda1c820f8cf
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-12-10 01:21:39 -0500

    Updates

---
+++ precache-manifest.5c43c6d935e9d8b9092b222c07bf2724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "807aea4048c3fb90c3dd",
+    "url": "/react-contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "807aea4048c3fb90c3dd",
+    "url": "/react-contact-manager/static/js/main.807aea40.chunk.js"
+  },
+  {
+    "revision": "f5fa0cd0884c2d3182ef",
+    "url": "/react-contact-manager/static/css/1.94a055db.chunk.css"
+  },
+  {
+    "revision": "f5fa0cd0884c2d3182ef",
+    "url": "/react-contact-manager/static/js/1.f5fa0cd0.chunk.js"
+  },
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "0da7332a82e75bb0b2d1757c90016db1",
+    "url": "/react-contact-manager/index.html"
+  }
+];
